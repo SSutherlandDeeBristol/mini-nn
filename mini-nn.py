845c0fdc071a480ca1355a212cbc704573fc89fb
SamSD <ss15060@my.bristol.ac.uk>
2019-06-03 14:08:18 +0100

    Reformatted label array

--- mini-nn.py
+++ mini-nn.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 import struct as st
 import numpy as np
 from matplotlib import pyplot as plt
 
 # Read in all of the training image data
-
 # Files downloaded from: http://yann.lecun.com/exdb/mnist/
 filename = {'images': 'data/train-images-idx3-ubyte',
             'labels': 'data/train-labels-idx1-ubyte'}
@@ -28,18 +28,25 @@ num_columns = st.unpack('>I', train_imagesfile.read(4))[0]
 num_labels = st.unpack('>I', train_labelsfile.read(4))[0]
 
 # Initialise the arrays for images and labels
-images_array = np.zeros((num_images, num_rows, num_columns))
-labels_array = np.zeros(num_labels)
+train_images_array = np.zeros((num_images, num_rows, num_columns))
+train_labels_array = np.zeros(num_labels)
 
 # Read in the pixel values
 num_bytes = num_images * num_rows * num_columns
-images_array = 255 - np.asarray(st.unpack('>' + 'B' * num_bytes,
+train_images_array = 255 - np.asarray(st.unpack('>' + 'B' * num_bytes,
                                           train_imagesfile.read(num_bytes))).reshape((num_images, num_rows, num_columns))
 
 # Read in the labels
-labels_array = np.asarray(
+train_labels_array = np.asarray(
     st.unpack('>' + 'B' * num_labels, train_labelsfile.read(num_labels)))
 
-print(labels_array[0])
-plt.imshow(images_array[0], cmap="gray")
-plt.show()
+# Reformat the labels into the expected format i.e label 1 = [0,1,0,0,0,0,0,0,0,0]
+train_output_array = np.zeros((num_labels, 10))
+
+for i in range(0, num_labels):
+    val = labels_array[i]
+    train_output_array[i][val] = 1
+
+#print(train_output_array[0])
+#plt.imshow(images_array[0], cmap="gray")
+#plt.show()
